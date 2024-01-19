d866ddee310b10fd8fa96ac17afaa37ef83ea125
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-01-19 11:57:07 -0300

    Exercicios do dia 1

---
+++ dia-1/teste_black.py
@@ -0,0 +1,12 @@
+felps = "pessoa instrutora"
+
+
+dicas_pythonicas_sao_legais = True
+
+
+if dicas_pythonicas_sao_legais:
+    print("Mais dicas por favor!!!")
+
+
+# Para formatar o arquivo de acordo com convenção do python:
+# python3 -m black teste_black.py
