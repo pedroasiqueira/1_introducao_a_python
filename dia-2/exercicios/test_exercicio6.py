e7fe24724451cb3fcb8d679f588c28d8d26de43b
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-01-25 22:29:55 -0300

    finalizndo testes exercicio 6

--- dia-2/exercicios/test_exercicio6.py
+++ dia-2/exercicios/test_exercicio6.py
@@ -13,3 +13,17 @@ def test_username_can_contain_letters_and_digits():
 
 def test_username_can_contain_letters_digits_and_dashes():
      assert validate_email('aa-1a@nomewebsite.ext') is None
+
+def test_username_can_contain_letters_digits_dashes_and_underscores():
+    assert validate_email('a_a1a-a@nomewebsite.ext') is None
+     
+def test_username_should_starts_with_letter():
+    assert validate_email('a@nomewebsite.ext') is None
+     
+def test_username_when_dont_start_with_letter_raise_exception():
+    with pytest.raises(ValueError):
+        validate_email('1@nomewebsite.ext')
+     
+def test_username_is_invalid_raise_exception():
+    with pytest.raises(ValueError):
+        validate_email('a%a@nomewebsite.ext')
