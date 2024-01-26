b45eddf7fa7d1c152f76a204dd44f4c86f9e15ef
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-01-25 22:50:51 -0300

    parte 1 do exercicio 6

--- dia-2/exercicios/exercicio6.py
+++ dia-2/exercicios/exercicio6.py
@@ -1,9 +1,22 @@
-# def validate_email(email):
+def validate_email(email):
+    index = 0
+    # validar primeira letra
+    if not email[index].isalpha():
+        raise ValueError("Username should starts with a letter")
 
-# validar primeira letra
+    # validar username
+    while email[index] != "@" and index < len(email):
+        letter = email[index]
+        if not letter.isalpha() and not letter.isdigit() and letter not in ("_", "-"):
+            raise ValueError(
+                "Username should contains only letters, "
+                "digits, dashes or underscores"
+            )
+        index += 1
+    index += 1
 
-# validar username
+    # validar website
 
-# validar website
+    # validar extensão
 
-# validar extensão
+    return None
