bde5d87fa90ede922cc6ef86f7811a56c6330ceb
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-02-01 13:35:24 -0300

    Escrita com Json

--- dia-2/write_JSON.py
+++ dia-2/write_JSON.py
@@ -1,7 +1,7 @@
 import json
 
 # leitura de todos os pokemons
-with open("pokemons.json") as file:
+with open("dia-2/pokemons.json") as file:
     pokemons = json.load(file)["results"]
 
 # Separamos somente os do tipo grama
@@ -10,7 +10,21 @@ for pokemon in pokemons:
     if "Grass" in pokemon["type"]:
         grass_type_pokemons.append(pokemon)
 
+
+#ESCREVENDO NO JSON, PRIMEIRA FORMA
+# Abre o arquivo para escrevermos apenas o pokemons do tipo grama
+with open("dia-2/with-arqCriados/grass_pokemonss-primeira-forma.json", "w") as file:
+    json_to_write = json.dumps(
+        grass_type_pokemons
+    )  # conversão de Python para o formato json (str)
+    file.write(json_to_write)
+
+
+#ESCREVENDO NO JSON, SEGUNDA FORMA
 # abre o arquivo para escrita
-with open("grass_pokemons.json", "w") as file:
+with open("dia-2/with-arqCriados/grass_pokemons-segunda-forma.json", "w") as file:
     # escreve no arquivo já transformando em formato json a estrutura
     json.dump(grass_type_pokemons, file)
+
+
+#Aparentemente as duas formas escrevem igual o arquivo
